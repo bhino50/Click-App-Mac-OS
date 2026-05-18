47351fce7cf1ead02ea225397386816330a3da92
Brandon <Hinojosab93@gmail.com>
2026-05-18 11:45:18 -0500

    Initial Commit

---
+++ Click/ClickApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClickApp.swift
+//  Click
+//
+//  Created by Brandon Hinojosa on 5/18/26.
+//
+
+import SwiftUI
+
+@main
+struct ClickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
